3e0ac620b31183dc765d99b9136e37edb2a494a9
Ruslan103 <117119513+Ruslan103@users.noreply.github.com>
2023-08-26 20:57:30 +0400

    устранил замечания

--- src/main/java/ru/practicum/shareit/booking/service/BookingService.java
+++ src/main/java/ru/practicum/shareit/booking/service/BookingService.java
@@ -2,7 +2,6 @@ package ru.practicum.shareit.booking.service;
 
 import ru.practicum.shareit.booking.dto.BookingDtoRequest;
 import ru.practicum.shareit.booking.dto.BookingDtoResponse;
-import ru.practicum.shareit.booking.model.State;
 
 import java.util.List;
 
