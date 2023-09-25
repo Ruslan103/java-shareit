bd24812fdbe6e72e1fc059fa2bd616b3df067aea
Ruslan103 <117119513+Ruslan103@users.noreply.github.com>
2023-09-25 21:13:29 +0400

    ТЗ 16

--- server/src/main/java/ru/practicum/shareit/ShareItGateway.java
+++ server/src/main/java/ru/practicum/shareit/ShareItGateway.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ShareItServer {
-
+public class ShareItGateway {
     public static void main(String[] args) {
-        SpringApplication.run(ShareItServer.class, args);
+        SpringApplication.run(ShareItGateway.class, args);
     }
-
 }
